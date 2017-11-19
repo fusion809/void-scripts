1b6db6ff5a3019181bacbc20c6f20cf4cc4899ef
fusion809 <brentonhorne77@gmail.com>
2017-11-19 15:25:23 +1000

    Updating xbps scripts

--- Shell/02-xbps.sh
+++ Shell/02-xbps.sh
@@ -14,5 +14,19 @@ alias xbup=update
 alias xbu=update
 
 function xbr {
+    sudo xbps-remove "$@"
+}
+
+function xbry {
     sudo xbps-remove -y "$@"
+}
+
+# List orphans
+function orp {
+    sudo xbps-query -O
+}
+
+# Autoremove
+function xbar {
+    xbr $(orp)
 }
