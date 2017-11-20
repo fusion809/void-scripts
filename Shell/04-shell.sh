5e9850429e6777fb19e8b46084cbedda94e382eb
fusion809 <brentonhorne77@gmail.com>
2017-11-20 12:57:26 +1000

    Adding zshf funct

--- Shell/04-shell.sh
+++ Shell/04-shell.sh
@@ -7,3 +7,10 @@ function sbash {
 function szsh {
     source $HOME/.zshrc
 }
+
+# Fix corrupted .zsh_history
+function zshf {
+    mv $HOME/.zsh_history $HOME/.zsh_history_bad
+    strings $HOME/.zsh_history_bad > $HOME/.zsh_history
+    fc -R $HOME/.zsh_history
+}
