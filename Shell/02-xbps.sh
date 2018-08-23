5bd816cafb62444ca57b01078e61fdae5365884b
Brenton Horne <brentonhorne77@gmail.com>
2018-08-23 19:04:58 +1000

    Updating common-scripts submodule

--- Shell/02-xbps.sh
+++ Shell/02-xbps.sh
@@ -50,16 +50,16 @@ ORAIV=$(cat /usr/lib/openra/VERSION)
 ORATV=$(cat $PK/void-packages-bleed/srcpkgs/openra-bleed/template | grep "version=" | cut -d '=' -f 2)
 COMT=$(cat $PK/void-packages-bleed/srcpkgs/openra-bleed/template | grep "commit=" | cut -d '=' -f 2)
 
-if ( ! [[ $ORAIV == "bleed-$ORATV" ]] ) || ( ! [[ -f /tmp/openra-update-lock ]] ) ; then
-	vared -p "You may wish to rebuild OpenRA as an update is available. Would you like to do that now?" -c yn
-	case $yn in
-		[yY]*)
-			obup $ORATV $COMT;;
-		[nN]*)
-			vared -p "OK, would you like to be re-asked when Zsh is re-started?" -c yn2
-			case $yn2 in
-				[nN]*)
-					touch /tmp/openra-update
-			esac
-	esac
-fi
+#if ( ! [[ $ORAIV == "bleed-$ORATV" ]] ) || ( ! [[ -f /tmp/openra-update-lock ]] ) ; then
+#	vared -p "You may wish to rebuild OpenRA as an update is available. Would you like to do that now?" -c yn
+#	case $yn in
+#		[yY]*)
+#			obup $ORATV $COMT;;
+#		[nN]*)
+#			vared -p "OK, would you like to be re-asked when Zsh is re-started?" -c yn2
+#			case $yn2 in
+#				[nN]*)
+#					touch /tmp/openra-update
+#			esac
+#	esac
+#fi
